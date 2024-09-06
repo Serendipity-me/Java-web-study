92b28899baea66442ed3c0a0b6f327e6a2e2dfef
lingqiye13579 <3042905903@qq.com>
2024-09-06 21:40:50 +0800

    first commit

---
+++ demo/src/main/java/HelloServlet.java
@@ -0,0 +1,11 @@
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+/**
+ * @author moqi
+ */
+
+@WebServlet(name ="helloServlet", value ="/hello-servlet")
+
+public class HelloServlet {
+}
